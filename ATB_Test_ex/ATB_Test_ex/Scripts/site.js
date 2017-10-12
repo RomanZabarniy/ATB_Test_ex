08716829a3154a1facaa8a03fea0146f44cf76fa
Roman Zabarniy <zaro.itstep@gmail.com>
2017-10-13 00:44:45 +0300

    добавлена библиотека knokcout.js

---
+++ ATB_Test_ex/ATB_Test_ex/Scripts/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    viewModelClients = new ClientsModel();
+    
+    ko.applyBindings(viewModelClients);
+    alert("Hi!");
+
+});
+
+function ClientsModel() {
+    var self = this;
+}
